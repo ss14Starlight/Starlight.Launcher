7c9fa08e906aff7cd1746772b7f64fd0e3eade89
Rinary <rinary.super@gmail.com>
2026-07-26 21:12:55 +0300

    localization in WebUI

---
+++ Starlight.Launcher.WebUI/Localization/ILocalizationManager.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+
+namespace Starlight.Launcher.WebUI.Localization;
+
+public interface ILocalizationManager
+{
+    event Action Changed { add; remove; }
+
+    string this[string key] { get; }
+
+    string GetString(string key);
+
+    string GetString(string key, params (string, object?)[] args);
+
+    void SwitchLanguage(string cultureName);
+
+    void SwitchLanguage(CultureInfo? culture);
+
+    List<string> EnumarateAllLoadedLanguages();
+}
