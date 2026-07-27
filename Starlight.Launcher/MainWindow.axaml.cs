d7d4754842eba8226c4c237314e374fc00fbafa3
Rinary <rinary.super@gmail.com>
2026-07-28 00:51:45 +0300

    fix scroll + dev tools

--- Starlight.Launcher/MainWindow.axaml.cs
+++ Starlight.Launcher/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using AvaloniaWebView;
 
@@ -9,17 +11,32 @@ public partial class MainWindow : Window
 {
     private readonly NativeWebView _web;
 
+    private readonly Uri? _blazorUrl;
+
     public MainWindow() : this(null) { }
 
     public MainWindow(Uri? blazorUrl)
     {
         InitializeComponent();
 
+        _blazorUrl = blazorUrl;
+
         _web = this.FindControl<NativeWebView>("Web")
             ?? throw new InvalidOperationException("WebView not found.");
 
         if (blazorUrl is not null)
             _web.Source = blazorUrl;
+
+#if DEBUG
+        Opened += (_, _) =>
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = _blazorUrl!.ToString(),
+                UseShellExecute = true
+            });
+        };
+#endif
     }
 
     private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
