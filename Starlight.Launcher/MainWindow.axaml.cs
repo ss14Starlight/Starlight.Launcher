164eb932eeaddee65601a0faa3504245349fedd6
Rinary <rinary.super@gmail.com>
2026-07-28 19:41:51 +0300

    totally experimental thingy

--- Starlight.Launcher/MainWindow.axaml.cs
+++ Starlight.Launcher/MainWindow.axaml.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
 
 namespace Starlight.Launcher;
 
@@ -20,6 +21,15 @@ public partial class MainWindow : Window
 
         _web = this.FindControl<NativeWebView>("Web")
             ?? throw new InvalidOperationException("WebView not found.");
+
+        if (OperatingSystem.IsLinux())
+        {
+            _web.EnvironmentRequested += (_, args) =>
+            {
+                if (args is LinuxWpeWebViewEnvironmentRequestedEventArgs wpeArgs)
+                    wpeArgs.PreferWebKitGtkInstead = true;
+            };
+        }
 
         if (blazorUrl is not null)
             _web.Source = blazorUrl;
